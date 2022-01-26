c5b557401c2d77225bb185ea474ec41dc4d0d2d9
Mariah Rosman <mrosman7@gmail.com>
2022-01-25 16:29:08 -0800

    adds firebase authorization

---
+++ firebase.js
@@ -0,0 +1,44 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_Ql73uYw8qna-w4hiiNHSlGHsw4QwRzg",
+  authDomain: "paw-planner.firebaseapp.com",
+  projectId: "paw-planner",
+  storageBucket: "paw-planner.appspot.com",
+  messagingSenderId: "187624919709",
+  appId: "1:187624919709:web:49b3de8e6b02bf0ac0ba74"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// // Import the functions you need from the SDKs you need
+// import * as firebase from "firebase";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD_Ql73uYw8qna-w4hiiNHSlGHsw4QwRzg",
+//   authDomain: "paw-planner.firebaseapp.com",
+//   projectId: "paw-planner",
+//   storageBucket: "paw-planner.appspot.com",
+//   messagingSenderId: "187624919709",
+//   appId: "1:187624919709:web:49b3de8e6b02bf0ac0ba74",
+// };
+
+// // Initialize Firebase
+// let app;
+// if (firebase.apps.length === 0) {
+//   app = firebase.initializeApp(firebaseConfig);
+// } else {
+//     app = firebase.app()
+// }
+
+// const auth = firebase.auth()
+
+// export { auth };
---
+++ screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>
+
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+})
+
+export default HomeScreen;
