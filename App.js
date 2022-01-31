1dc3ad1ff247c18d7963ca4fcdb36c063214d4e0
Mariah Rosman <mrosman7@gmail.com>
2022-01-31 12:16:26 -0800

    minor formatting changes in App.js

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
-import HomeScreen from './screens/HomeScreen';
-import { app } from './firebase.js';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/LoginScreen";
+import HomeScreen from "./screens/HomeScreen";
+import { app } from "./firebase.js";
 import PetsListScreen from "./screens/PetsListScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <PetsListScreen/>
-    // <NavigationContainer>
-    //   <Stack.Navigator>
-    //     <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
-    //     <Stack.Screen name="Home" component={HomeScreen} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={LoginScreen}
+        />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
