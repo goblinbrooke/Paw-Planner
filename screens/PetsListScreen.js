9e6f7d7788085bab14d924857b610e5eaa149092
Mariah Rosman <mrosman7@gmail.com>
2022-02-03 17:17:36 -0800

    refactors PetsListScreen

--- screens/PetsListScreen.js
+++ screens/PetsListScreen.js
@@ -3,28 +3,31 @@ import {
   View,
   FlatList,
   StyleSheet,
+  SafeAreaView,
   Text,
-  Image,
   ActivityIndicator,
+  Button,
 } from "react-native";
 
-// list that holds all of the pet components
-
 function PetsListScreen(props) {
+  // state variables
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
+  // database endpoint
   const petsDB =
     "https://paw-planner-default-rtdb.firebaseio.com/user/123/pets.json";
 
   const handlePets = async () => {
-    console.log("in the handlePets function");
     try {
       // console.log("in the try block");
       const response = await fetch(petsDB);
       const json = await response.json();
-      console.log(json);
+      // console.log("This is the object:", json);
+      // console.log("------------------------------------------");
       setData(json);
+      // console.log("Trying to print here:", json[id].name);
+      // console.log("------------------------------------------");
     } catch (error) {
       console.log("There is an error!");
       console.error(error);
@@ -37,6 +40,51 @@ function PetsListScreen(props) {
     handlePets();
   }, []);
 
+  const petInfo = (data) => {
+    const petData = [];
+    for (let pet in data) {
+      petData.push(data[pet]);
+      // console.log(data[pet].name);
+    }
+    return petData;
+  };
+
+  console.log(petInfo(data));
+
+  //   const DATA = {
+  //     id1: {
+  //       id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+  //       name: "First Item",
+  //     },
+  //     id2: {
+  //       id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+  //       name: "Second Item",
+  //     },
+  //     id3: {
+  //       id: "58694a0f-3da1-471f-bd96-145571e29d72",
+  //       name: "Third Item",
+  //     },
+  //   };
+
+  //   const Item = ({ name }) => (
+  //     <View style={styles.item}>
+  //       <Text style={styles.title}>{name}</Text>
+  //     </View>
+  //   );
+
+  //   const renderItem = ({ item }) => <Item name={item.name} />;
+
+  //   return (
+  //     <SafeAreaView style={styles.container}>
+  //       {/* <FlatList
+  //         data={DATA}
+  //         renderItem={renderItem}
+  //         keyExtractor={(item) => item.id}
+  //       /> */}
+  //     </SafeAreaView>
+  //   );
+  // }
+
   return (
     <View style={styles.container}>
       {isLoading ? (
@@ -44,7 +92,7 @@ function PetsListScreen(props) {
       ) : (
         <FlatList
           keyExtractor={(item) => item.id}
-          data={data}
+          data={petInfo}
           renderItem={({ item }) => (
             <Text style={styles.item}>{item.name}</Text>
           )}
